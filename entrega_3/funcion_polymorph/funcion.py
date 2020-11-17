58cda191205671414d3d2ed691d2643f36d1308c
$Mexcel <65205530+mexcelzapata@users.noreply.github.com>
2020-11-17 01:43:54 -0300

    Update funcion.py

--- entrega_3/funcion_polymorph/funcion.py
+++ entrega_3/funcion_polymorph/funcion.py
@@ -1,16 +1,16 @@
-def funcion_throughput(packet):
-    import time as t
-    if (packet['TCP']['dstport'] == 5432):
-       try:
-          time = int(t.time())
-          try:
-             packet.mapa[time] = packet.mapa[time] + (packet['IP']['len'] + 14)*8
-          except:
-             packet.mapa[time] = (packet['IP']['len'] + 14)*8
-          print (packet.mapa)
-       except:
-          map = {}
-          packet.global_var('mapa',map)
-          packet.mapa[time] = (packet['IP']['len'] + 14)*8
-          print (packet.mapa)
-    return packet
+def delay_2(packet):
+        import time as tiempo
+        if (packet['TCP']['dstport'] == 5432):
+                try:
+                        time = int(tiempo.time())
+                        try:
+                                packet.conjunto[time] = packet.conjunto[time] + (packet['IP']['len'] + 14 ) * 8
+                        except:
+                                packet.conjunto[time] = (packet['IP']['len'] + 14 ) * 8
+                        print (packet.conjunto)
+                except:
+                        repeticiones = {}
+                        packet.global_var('conjunto',repeticiones)
+                        packet.conjunto[time] = (packet['IP']['len'] + 14 ) * 8
+                        print (packet.conjunto)
+        return packet
